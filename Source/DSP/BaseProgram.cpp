9e8678f116a9bd5fa80ba67fdba774bd8f9e5bbb
masterglob <masterglob@gmail.com>
2025-06-14 17:52:04 +0200

    Fix overflow

--- Source/DSP/BaseProgram.cpp
+++ Source/DSP/BaseProgram.cpp
@@ -11,6 +11,12 @@
 
 namespace
 {
+    inline LineValue toCCValue(float v)
+    {
+        if (v < 0.0f) return 0.0f;
+        if (v > 127.0) return 127;
+        return TO_LINE_VALUE(v);
+    }
 }
 
 DefaultProgram& DefaultProgram::instance(void)
@@ -25,22 +31,22 @@ BaseProgram::Events DefaultProgram::execute(const LedVect& leds, const Parameter
     result.reserve(256);
 
     static const float coef(127);
-    const float mRed(parameterManager.getMainRed());
-    const float mGreen(parameterManager.getMainGreen());
-    const float mBlue(parameterManager.getMainBlue());
-    const float mWhite(parameterManager.getMainWhite());
-    // const float mHue(parameterManager.getMainHue());
 
+    const float mRed(parameterManager.getMainRed() * coef);
+    const float mGreen(parameterManager.getMainGreen() * coef);
+    const float mBlue(parameterManager.getMainBlue() * coef);
+    const float mWhite(parameterManager.getMainWhite() * coef);
+    // const float mHue(parameterManager.getMainHue());
 
     for (const Led_RGBW* pLed : leds)
     {
         if (!pLed) continue;
         const Led_RGBW& led(*pLed);
-        result.emplace_back(led.mr, static_cast<unsigned char> (mRed * coef));
-        result.emplace_back(led.mg, static_cast<unsigned char> (mGreen * coef));
-        result.emplace_back(led.mb, static_cast<unsigned char> (mBlue * coef));
+        result.emplace_back(led.mr, toCCValue(mRed));
+        result.emplace_back(led.mg, toCCValue(mGreen));
+        result.emplace_back(led.mb, toCCValue(mBlue));
         const float fw = (mBlue + mGreen + mRed) * mWhite * coef;
-        result.emplace_back(led.mw, static_cast<unsigned char> (fw));
+        result.emplace_back(led.mw, toCCValue(fw));
     }
     return result;
 }
