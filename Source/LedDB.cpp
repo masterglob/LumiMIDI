484c5a2e0829ce96117a3ac623d91b515cfd7750
masterglob <masterglob@gmail.com>
2026-04-18 14:49:49 +0200

    Replace LEDs

--- Source/LedDB.cpp
+++ Source/LedDB.cpp
@@ -19,27 +19,25 @@ const LedCtrlLine Led_Line_SQs(81, 2);
 
 static const int LedWDemo(550);
 const LedContext Led_G{"G", Led_Line_G, Rect(100, 100, 100, -50)};
-const LedContext Led_M{"M", Led_Line_M, Rect(100, 150, 150, 100)};
-const LedContext Led_Sh1{"Sh1", Led_Line_Sh1, Rect(100, 120, 0, 130)};
-const LedContext Led_Sh2{"Sh2", Led_Line_Sh2, Rect(200, 70, 0, 130)};
+const LedContext Led_M{"M", Led_Line_M, Rect(100, 120, 0, 130)};
+const LedContext Led_Sh1{"Sh1", Led_Line_Sh1, Rect(170, 200, 0, 130)};
+const LedContext Led_Sh2{"Sh2", Led_Line_Sh2, Rect(160, 160, 0, 130)};
 const LedContext Led_SQ{"SQ", Led_Line_SQ, Rect(280, 200, 0, 200)};
 
-const LedContext Led_Gs{"Gs", Led_Line_Gs, Rect(LedWDemo - 100, 100, -100, -50)};
-const LedContext Led_Ms{"Ms", Led_Line_Ms, Rect(LedWDemo - 100, 150, -150, 100)};
-const LedContext Led_Sh1s{"Sh1s", Led_Line_Sh1s, Rect(LedWDemo - 100, 120, 0, 130)};
-const LedContext Led_Sh2s{"Sh2s", Led_Line_Sh2s, Rect(LedWDemo - 200, 70, 0, 130)};
-const LedContext Led_SQs{"SQs", Led_Line_SQs, Rect(LedWDemo - 280, 200, 0, 200)};
+const LedContext Led_Gs{"Gs", Led_Line_Gs,
+                        Rect(LedWDemo - 100, 100, -100, -50)};
+const LedContext Led_Ms{"Ms", Led_Line_Ms, Rect(LedWDemo - 100, 120, 0, 130)};
+const LedContext Led_Sh1s{"Sh1s", Led_Line_Sh1s,
+                          Rect(LedWDemo - 170, 200, 0, 130)};
+const LedContext Led_Sh2s{"Sh2s", Led_Line_Sh2s,
+                          Rect(LedWDemo - 160, 160, 0, 130)};
+const LedContext Led_SQs{"SQs", Led_Line_SQs,
+                         Rect(LedWDemo - 280, 200, 0, 200)};
 
 const ConstLedVect demoLeds{
-    &Led_G,
-    &Led_M,
-    &Led_Sh1,
-    &Led_Sh2,
+    &Led_G,   &Led_M,  &Led_Sh1,  &Led_Sh2,
     &Led_SQ,  // Side 1
-    &Led_Gs,
-    &Led_Ms,
-    &Led_Sh1s,
-    &Led_Sh2s,
+    &Led_Gs,  &Led_Ms, &Led_Sh1s, &Led_Sh2s,
     &Led_SQs,  // Side 2
 };
 
@@ -86,14 +84,16 @@ void LedDB::removeLed(const LedId ledId) {
 LedContext* LedDB::getLed(const LedId ledId) {
   auto it(mLedsMapEdit.find(ledId));
 
-  if (it == mLedsMapEdit.end()) return nullptr;
+  if (it == mLedsMapEdit.end())
+    return nullptr;
   return it->second;
 }
 /*************************************************/
 const LedContext* LedDB::getLed(const LedId ledId) const {
   auto it(mLedsMapEdit.find(ledId));
 
-  if (it == mLedsMapEdit.end()) return nullptr;
+  if (it == mLedsMapEdit.end())
+    return nullptr;
   return it->second;
 }
 
